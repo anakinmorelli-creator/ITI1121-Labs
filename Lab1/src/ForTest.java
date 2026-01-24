b8915cd3a5e92dd7f3107d221d654ac9a3b8b21c
Anakin Morelli <anakinmorelli@gmail.com>
2026-01-24 02:37:00 -0500

    Finished ForTest

---
+++ Lab1/src/ForTest.java
@@ -0,0 +1,12 @@
+public class ForTest {
+    public static void main(String[] args) {
+        int i;
+        int sum = 0;
+
+        for (i = 1; i <= 10; i++) {
+            sum = sum + i;
+        }
+
+        System.out.println("The some of the first 10 integers is " + sum);
+    }
+}
