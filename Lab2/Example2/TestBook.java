a21bc22da46a7ed901ba919f4c3dcdba1331f16c
Anakin Morelli <anakinmorelli@gmail.com>
2026-01-30 22:46:46 -0500

    Finished Example 2 on Laptop

--- Lab2/Example2/TestBook.java
+++ Lab2/Example2/TestBook.java
@@ -1,7 +1,10 @@
 public class TestBook {
     public static void main(String[] agrs) {
         Book book1 = new Book("J.K Rowlling", "Harry Potter");
-        Book book2 = new Book("Taylor Jenkins Reid", "Atmosphere");
+        Book book2 = new Book();
+
+        book2.setAuthor("Taylor Jenkins Reid");
+        book2.setTitle("Atmosphere");
 
         System.out.println(book1.getAuthor());
         System.out.println(book2.getAuthor());
